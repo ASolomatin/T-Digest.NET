9f8dcb135392205f6d02199d5429a50dd523602f
Aleksej Solomatin <aleksejsolomatin@gmail.com>
2023-09-05 15:47:20 +0300

    Updated example app

--- TDigestNet.CMD/Program.cs
+++ TDigestNet.CMD/Program.cs
@@ -1,17 +1,22 @@
-﻿using TDigestNet;
+﻿using System.Diagnostics;
+using TDigestNet;
 
-Random r = new Random();
+Random random = new();
+TDigest digest = new();
+Stopwatch stopwatch = new();
 
-TDigest digest = new TDigest();
-
-for (int i = 0; i < 10000; i++)
+stopwatch.Start();
+for (int i = 0; i < 1000000; i++)
 {
-    var n = (r.Next() % 50) + (r.Next() % 50);
+    var n = random.NextDouble() * 100;
     digest.Add(n);
 }
+stopwatch.Stop();
 
 Console.WriteLine($"Average: {digest.Average}");
 Console.WriteLine($"Percentile 10: {digest.Quantile(10 / 100d)}");
 Console.WriteLine($"Percentile 50: {digest.Quantile(50 / 100d)}");
 Console.WriteLine($"Percentile 80: {digest.Quantile(80 / 100d)}");
 Console.WriteLine($"Percentile 99: {digest.Quantile(99 / 100d)}");
+Console.WriteLine($"Time spent for 1M samples: {stopwatch.Elapsed}");
+
